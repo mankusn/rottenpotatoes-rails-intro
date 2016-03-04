ed42085874f8487e37b3d633658bdf882c308c79
mankusn <mankusnick@yahoo.com>
2016-03-03 19:57:37 -0600

    Update movies_helper.rb

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,13 +3,6 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-  def sort_column
-    Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
 end
 
 
